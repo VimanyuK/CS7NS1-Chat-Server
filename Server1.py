7b24dd8ce38594a92a83f6056f42d4dd0b50541a
vimanyu <vimanyukachroo@yahoo.com>
2017-11-23 12:56:05 +0000

    Add files via upload
    Join chatroom function with acknowledgements.

--- Server1.py
+++ Server1.py
@@ -15,7 +15,44 @@ import sys
 threadLock = Lock()
 
 def join(conn_msg,csock):
-    pass
+    
+    threadLock.acquire()
+    gname = conn_msg.find('JOIN_CHATROOM:'.encode('utf-8'))+15
+    gname_end = conn_msg.find('\n'.encode('utf-8'))
+    groupname = conn_msg[gname:gname_end]
+    
+    cname = conn_msg.find('CLIENT_NAME'.encode('utf-8'))+13
+    cname_end = conn_msg.find(' '.encode('utf-8'),cname)
+    clientname = conn_msg[cname:cname_end]
+    rID = 0
+    if (groupname.decode('utf-8')) == 'room1' :
+        g1_clients.append(clThread.socket)
+        rID = 1001
+    elif groupname == 'room2' :
+       g2_clients.append(clThread.socket)
+       rID = 1002
+	#sending ackowledgement
+    response = "JOINED_CHATROOM: ".encode('utf-8') + groupname+ "\n".encode('utf-8')
+    response += "SERVER_IP: ".encode('utf-8') + host.encode('utf-8') + "\n".encode('utf-8')
+    response += "PORT:".encode('utf-8') + str(port).encode('utf-8') + "\n".encode('utf-8')
+    response += "ROOM_REF: ".encode('utf-8') + str(rID).encode('utf-8') +'\n'.encode('utf-8')
+    response += "JOIN_ID: ".encode('utf-8') + str(clThread.uid).encode('utf-8') + "\n".encode('utf-8')
+    csock.send(response)
+    grpmessage = "CHAT:".encode('utf-8') + str(rID).encode('utf-8') + "\n".encode('utf-8')
+    grpmessage += "CLIENT_NAME:".encode('utf-8') + clientname + "\n".encode('utf-8') 
+    grpmessage += "MESSAGE:".encode('utf-8') + clientname + "\n".encode('utf-8') 
+    grpmessage += "CLIENT_ID:".encode('utf-8') + str(clThread.uid).encode('utf-8') +"\n".encode('utf-8')
+    grpmessage += "JOINED_GROUP".encode('utf-8') +"\n".encode('utf-8')
+    if (groupname.decode('utf-8')) == 'room1':
+        for x in range(len(g1_clients)):
+            g1_clients[x].send(grpmessage)
+    elif (groupname.decode('utf-8')) == 'room2':
+        for x in range(len(g2_clients)):
+            g2_clients[x].send(grpmessage)
+        threadLock.release()
+    return groupname,clientname,rID
+
+
 def chat(conn_msg,csock):
     pass
 def resp(msg,socket):
