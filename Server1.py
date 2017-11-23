d7107b5219446c33c815a4fc06e037a3deca7f37
vimanyu <vimanyukachroo@yahoo.com>
2017-11-23 03:45:19 +0000

    Add files via upload
    implementing threads for the clients.

--- Server1.py
+++ Server1.py
@@ -8,35 +8,49 @@
 
 import socket
 from threading import Thread
+from threading import Lock
 import random
 import sys
 
+#creating threads for clients   
+class client_threads(Thread):
+
+	def __init__(self,ip,port,socket):
+		Thread.__init__(self)
+		self.ip = ip
+		self.port = port
+		self.chatroom =[] 
+		self.socket = socket
+		self.uid = random.randint(1000,2000)
+		self.roomname = ''
+		self.clientname = ''
+		self.roomID = ''
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
+host = socket.gethostbyname(socket.gethostname())
 port = 5555
-print ('Socket created')
- 
 #Bind socket to local host and port
 try:
     server.bind((host, port))
 except socket.error as msg:
     print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()     
-print ('Socket bind complete')
 
+thread_count = [] 
+g1_clients = []
+g2_clients = []
+print('Server started')
 print("Host name: ",host)
 
 #Start listening on socket
 while True:
     server.listen(5)
-    conn, ip = server.accept()
-    #monitoring connections
-    conn.send("Connected to the server".encode('utf-8') )
+    (conn,(ip,port)) = server.accept()
     print("Connected to ",ip)
-    data = conn.recv(2048)
-   # printing the message received from the client
-    print(data)
-   
-
-
+    clThread = client_threads(ip,port,conn)
+    clThread.start()
+    thread_count.append(clThread)
+    
+    
+    
